2dee3986c098960b1444c01ffea9a2af96f4a40f
cryolins <hepeilin07@gmail.com>
2026-02-07 17:00:51 -0500

    shadcn-svelte installation

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full h-screen bg-zinc-800">
+    
+</div>
